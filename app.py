8b3bc133e59bf8537e2293d0a601f02f411657d1
massed-spec <massed@smail.ucas.edu.ps>
2026-03-12 21:00:03 +0200

    app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.sidebar.write("2 = Agricu")
 st.sidebar.write("3 = Water")
 
 # عنوان التطبيق
-st.title("Raster Classification using Decision Tree _ محمد سليم أبو اسد ")
+st.title("Raster Classification using Decision Tree ")
 st.title("محمد سليم أبو اسد ")
 
 # رفع الصورة
